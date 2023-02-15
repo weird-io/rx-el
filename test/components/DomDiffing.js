6ffe9c8b35e79d9a9fb739470a5fdfd7a5aa8b3f
nik-weird <nik@weird.io>
2023-02-15 17:03:48 +0100

    feat: disable dom-diffing by adding html attribute + refactor

--- test/components/DomDiffing.js
+++
@@ -1,6 +0,0 @@
-import DomDiffingReactiveHTMLElement from '/src/reactive/DomDiffingReactiveHTMLElement.js';
-import Test from '../Test.js';
-
-class DomDiffingComponent extends Test(DomDiffingReactiveHTMLElement) {}
-
-customElements.define('dom-diffing-component', DomDiffingComponent);
